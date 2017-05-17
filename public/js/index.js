3bc3fa289e6b9ac9b4c71a9a1dfcfb04ca3b0be3
Adrian Marikar <adrianmarikar@gmail.com>
2017-05-17 21:49:05 +0100

    Scroll to bottom to if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToBottom () {
+	// Selectors
+	var messages = jQuery('#messages');
+	var newMessage = messages.children('li:last-child')
+	// Heights
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+	var newMessageHeight = newMessage.innerHeight();
+	var lastMessageHeight = newMessage.prev().innerHeight();
+
+	if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+		messages.scrollTop(scrollHeight);
+	}
+}
+
+
 		socket.on('connect', function () {
 			console.log('Connected to server');
 		});
@@ -7,7 +24,6 @@ var socket = io();
 		socket.on('disconnect', function () {
 			console.log('Disconnected from server');
 		});
-
 
 		socket.on('newMessage', function (message) {
 			var formattedTime = moment(message.createdAt).format('h:mm a')
@@ -20,6 +36,7 @@ var socket = io();
 			});
 
 			jQuery('#messages').append(html);
+			scrollToBottom();
 		});
 
 		socket.on('newLocationMessage', function (message) {
@@ -33,6 +50,7 @@ var socket = io();
 			});
 
 			jQuery('#messages').append(html);
+			scrollToBottom();
 		});
 
 
